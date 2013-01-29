c15015ee980fbbf0f636f58f14a4ca55894c01e6
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2013-01-29 09:01:30 +0100

    I am slowly moving towards adopting pep 8 throughout the code. As a result many files have been renamed, hence the deletions.

--- src/expWorkbench/uncertaintySpace.py
+++
@@ -1,5 +0,0 @@
-from uncertainties import  *
-from samplers import *
-
-raise DeprecationWarning("uncertaintySpace is deprecated, use the modules\
-                          uncertainties and sampler instead")
