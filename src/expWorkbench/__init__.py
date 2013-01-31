10a07a6c47c24aa2e8b6c86cde57242c6245aae5
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2013-01-31 21:28:57 +0100

    svn id removal

--- src/expWorkbench/__init__.py
+++ src/expWorkbench/__init__.py
@@ -1,11 +1,8 @@
-SVN_ID = '$Id: __init__.py 1029 2012-11-21 20:12:09Z jhkwakkel $'
 from ema_exceptions import * 
-
+from ema_logging import *
 from model import *
 from model_ensemble import *
 from outcomes import *
 from uncertainties import *
 from samplers import *
 from util import *
-
-
