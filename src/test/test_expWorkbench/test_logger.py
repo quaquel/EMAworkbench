b3fd34fea87c71b96850a569ec1c5de83be2ad57
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2013-03-11 11:13:35 +0100

    logging test code, very rudimentary, merely a place holder for now

---
+++ src/test/test_expWorkbench/test_logger.py
@@ -0,0 +1,9 @@
+'''
+Created on 11 Mar 2013
+
+@author: jhkwakkel
+'''
+from expWorkbench import ema_logging
+
+ema_logging.log_to_stderr(ema_logging.INFO)
+ema_logging.log_to_stderr(ema_logging.INFO)
