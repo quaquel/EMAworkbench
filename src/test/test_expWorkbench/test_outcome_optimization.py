720f1d099757a81b5f15fc155e7b96508e3c77e3
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2013-10-04 10:11:02 +0200

    update to quick test of outcome optimization

--- src/test/test_expWorkbench/test_outcome_optimization.py
+++ src/test/test_expWorkbench/test_outcome_optimization.py
@@ -5,6 +5,7 @@ Created on Feb 28, 2012
 '''
 from __future__ import division
 import numpy as np
+import matplotlib.pyplot as plt
 
 from expWorkbench import ema_logging
 from test.test_vensim_flu import FluModel
@@ -29,7 +30,7 @@ def test_optimization():
     ensemble.set_model_structure(model)
     ensemble.parallel=True
         
-    stats, pop  = ensemble.perform_outcome_optimization(obj_function = obj_function_multi,
+    stats_callback, pop  = ensemble.perform_outcome_optimization(obj_function = obj_function_multi,
                                                     reporting_interval=100, 
                                                     weights=(MAXIMIZE, MAXIMIZE),
                                                     pop_size=100,
@@ -37,22 +38,36 @@ def test_optimization():
                                                     crossover_rate=0.5,
                                                     mutation_rate=0.05,
                                                     caching=False)
-    res = stats.hall_of_fame.keys
     
-    print len(stats.tried_solutions.values())
-#    
-#    x = [entry.values[0] for entry in res]
-#    y = [entry.values[1] for entry in res]
-#    
-#    print len(x), len(y)
-#    
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111)
-#    ax.scatter(x,y)
-#    ax.set_ylabel("deceased population")
-#    ax.set_xlabel("infected fraction")
-#    
-#    plt.show()
+    res = stats_callback.hall_of_fame.keys
+
+    x = [entry.values[0] for entry in res]
+    y = [entry.values[1] for entry in res]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x,y)
+    ax.set_ylabel("deceased population")
+    ax.set_xlabel("infected fraction")
+
+    change = stats_callback.change
+    added = [entry[0] for entry in change]
+    removed = [entry[1] for entry in change]
+    e_progress = [entry[2] for entry in change]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(added, label='added')
+    ax.plot(removed, label='removed')
+    ax.set_ylabel("deceased population")
+    ax.set_xlabel("infected fraction")
+    ax.legend(loc='best')
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(e_progress, label='$\epsilon$ progress')   
+    
+    plt.show()
 
 if __name__ == '__main__':
     test_optimization()
