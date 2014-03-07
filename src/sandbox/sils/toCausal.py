fb7e751a45f015992b88aa9ce612577393362235
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-03-07 13:25:07 +0100

    additional except clause in pyNetlogo
    engine exception now also being transformed into a netlogo exception

--- src/sandbox/sils/toCausal.py
+++ src/sandbox/sils/toCausal.py
@@ -7,7 +7,7 @@ Created on Dec 7, 2011
 import networkx as nx
 import matplotlib.pyplot as plt
 import cPickle
-from expWorkbench.vensimDLLwrapper import VensimWarning
+from connectors.vensimDLLwrapper import VensimWarning
 
 try:
     from networkx import graphviz_layout
@@ -15,8 +15,8 @@ except ImportError:
     raise ImportError("This example needs Graphviz and either PyGraphviz or Pydot")
 
 
-from expWorkbench import vensim
-from expWorkbench import vensimDLLwrapper as venDLL
+from connectors import vensim
+from connectors import vensimDLLwrapper as venDLL
 
 vensim.load_model(r'C:\workspace\EMA-workbench\src\sandbox\sils\MODEL.vpm')
 
