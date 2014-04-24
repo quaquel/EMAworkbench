0181c3dc86db32a3f1669dd8f540ba43ba185028
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-04-24 20:19:26 +0200

    energy transition test data added

--- src/test/util.py
+++ src/test/util.py
@@ -90,7 +90,93 @@ def load_scarcity_data():
     outcomes[TIME] = np.loadtxt(fn, delimiter=',')
     
     return experiments, outcomes
+
+def load_eng_trans_data():
+    path = os.path.dirname(__file__)
     
+    fn = './data/eng_trans/experiments.csv'
+    fn = os.path.join(path, fn)
+    experiments = csv2rec(fn)
+    outcomes = {}
+        
+    dt_descr = [('ini PR T4', '<f8'),
+                ('lifetime T1', '<f8'),
+                ('lifetime T2', '<f8'),
+                ('lifetime T3', '<f8'),
+                ('lifetime T4', '<f8'),
+                ('ec gr t1', '<f8'),
+                ('ec gr t2', '<f8'),
+                ('ec gr t3', '<f8'),
+                ('ec gr t4', '<f8'),
+                ('ec gr t5', '<f8'),
+                ('ec gr t6', '<f8'),
+                ('ec gr t7', '<f8'),
+                ('ec gr t8', '<f8'),
+                ('ec gr t9', '<f8'),
+                ('ec gr t10', '<f8'),
+                ('random PR min', '<f8'),
+                ('random PR max', '<f8'),
+                ('seed PR T1', '<i4'),
+                ('seed PR T2', '<i4'),
+                ('seed PR T3', '<i4'),
+                ('seed PR T4', '<i4'),
+                ('absolute preference for MIC', '<f8'),
+                ('absolute preference for expected cost per MWe', '<f8'),
+                ('absolute preference against unknown', '<f8'),
+                ('absolute preference for expected progress', '<f8'),
+                ('absolute preference against specific CO2 emissions', '<f8'),
+                ('performance expected cost per MWe T1', '<f8'),
+                ('performance expected cost per MWe T2', '<f8'),
+                ('performance expected cost per MWe T3', '<f8'),
+                ('performance expected cost per MWe T4', '<f8'),
+                ('performance CO2 avoidance T1', '<f8'),
+                ('performance CO2 avoidance T2', '<f8'),
+                ('performance CO2 avoidance T3', '<f8'),
+                ('performance CO2 avoidance T4', '<f8'),
+                ('SWITCH T3', '|O4'),
+                ('SWITCH T4', '|O4'),
+                ('preference switches', '|O4'),
+                ('ini cap T1', '<f8'),
+                ('ini cap T2', '<f8'),
+                ('ini cap T3', '<f8'),
+                ('ini cap T4', '<f8'),
+                ('ini cost T1', '<f8'),
+                ('ini cost T2', '<f8'),
+                ('ini cost T3', '<f8'),
+                ('ini cost T4', '<f8'),
+                ('ini cum decom cap T1', '<f8'),
+                ('ini cum decom cap T2', '<f8'),
+                ('ini cum decom cap T3', '<f8'),
+                ('ini cum decom cap T4', '<f8'),
+                ('average planning and construction period T1', '<f8'),
+                ('average planning and construction period T2', '<f8'),
+                ('average planning and construction period T3', '<f8'),
+                ('average planning and construction period T4', '<f8'),
+                ('ini PR T1', '<f8'),
+                ('ini PR T2', '<f8'),
+                ('ini PR T3', '<f8'),
+                ('model', '|O4'),
+                ('policy', '|O4')]
+    
+    
+    dt = np.dtype(dt_descr)
+    experiments = experiments.astype(dt)
+  
+    fn = './data/eng_trans/total capacity installed.csv'
+    fn = os.path.join(path, fn)    
+    outcomes['total capacity installed'] = np.loadtxt(fn, delimiter=',')
+    
+    fn = './data/eng_trans/time.csv'
+    fn = os.path.join(path, fn)
+    outcomes[TIME] = np.loadtxt(fn, delimiter=',')
+
+    fn = './data/eng_trans/total fraction new technologies.csv'
+    fn = os.path.join(path, fn)
+    outcomes['total fraction new technologies'] = np.loadtxt(fn, delimiter=',')
+    
+    return experiments, outcomes
+  
 if __name__ == '__main__':
     load_flu_data()
     load_scarcity_data()
+    load_eng_trans_data()
