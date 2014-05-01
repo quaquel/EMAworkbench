0b7821de731e051ab5018d3d91b84e975815f3d4
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-05-01 14:32:24 +0200

    disable some imports

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,8 +1,8 @@
 #import graphs3d
-import interactive_graphs
+# import interactive_graphs
 # import orange_functions
 import prim
-import scenario_discovery
+# import scenario_discovery
 import b_and_w_plotting
 import plotting
 import plotting_util
