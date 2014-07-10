7a7b4e4b0e03a0ea63a2561b649e24f0e04c48d9
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-07-10 14:14:34 +0200

    small ff sampler test update

--- src/test/test_expWorkbench/test_samplers.py
+++ src/test/test_expWorkbench/test_samplers.py
@@ -12,7 +12,7 @@ def test_generate_samples(sampler):
     unc_3 = CategoricalUncertainty(['a','b', 'c'], "3")
     uncertainties = [unc_1, unc_2, unc_3]
     
-    return sampler.generate_samples(uncertainties, 100)
+    return sampler.generate_samples(uncertainties, 10)
 
 def test_generate_designs(sampler):
     unc = test_generate_samples(sampler)
@@ -23,12 +23,15 @@ def test_sampler(sampler):
     samples = test_generate_samples(sampler)
 
     for key in sorted(samples.keys()):
-        print key.name, samples[key]
+        print key, samples[key]
         
     designs = test_generate_designs(sampler)
+    temp_designs = set()
     for entry in designs:
         print entry
+        temp_designs.add(entry)
     print sampler.deterimine_nr_of_designs(samples)
+    print len(temp_designs)
 
 def test_lhs_samper():
     sampler = LHSSampler()
@@ -43,6 +46,6 @@ def test_ff_sampler():
     test_sampler(sampler)
 
 if __name__ == "__main__":
-    test_lhs_samper()
-    test_mc_sampler()
+#     test_lhs_samper()
+#     test_mc_sampler()
     test_ff_sampler()
