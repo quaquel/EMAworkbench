eec39a1537a714843a394e52ad9119c3c144c661
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-07-13 20:55:00 +0200

    lasso based feature scoring start

--- src/test/test_analysis/test_feature_scoring.py
+++ src/test/test_analysis/test_feature_scoring.py
@@ -10,10 +10,10 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 from expWorkbench import ema_logging, load_results
-from analysis import feature_selection as fs
+from analysis import feature_scoring as fs
 from sklearn.ensemble.forest import RandomForestRegressor
 
-class FeatureSelectionTestCase(unittest.TestCase):
+class FeatureScoringTestCase(unittest.TestCase):
     def test_prepare_experiments(self):
         x = np.array([(0,1,2,1),
                       (2,5,6,1),
@@ -22,7 +22,7 @@ class FeatureSelectionTestCase(unittest.TestCase):
                             ('b', np.float),
                             ('c', np.float),
                             ('d', np.float)])
-        x = fs._preperate_experiments(x)
+        x = fs._prepare_experiments(x)
         
         correct = np.array([[0,1,2,1],
                             [2,5,6,1],
@@ -37,7 +37,7 @@ class FeatureSelectionTestCase(unittest.TestCase):
         x['a'] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
         x['b'] = [0,1,2,3,4,5,6,7,8,9]
         x['c'] = ['a','b','a','b','a','a','b','a','b','a', ]
-        x = fs._preperate_experiments(x)
+        x = fs._prepare_experiments(x)
        
         correct = np.array([[0.1, 0, 0],
                             [0.2, 1, 1],
@@ -52,11 +52,64 @@ class FeatureSelectionTestCase(unittest.TestCase):
                             ], dtype=np.float)
 
         self.assertTrue(np.all(x==correct))
+
+
+    def test_prepare_outcomes(self):
+        fn = r'../data/1000 flu cases no policy.tar.gz'
+        results = load_results(fn)
         
+        # string type correct
+        ooi = 'nr deaths'
+        results[1][ooi] = results[1]['deceased population region 1'][:,-1]
+        y, categorical = fs._prepare_outcomes(results[1], ooi)
+        
+        self.assertFalse(categorical)
+        self.assertTrue(len(y.shape)==1)
+        
+        # string type not correct --> KeyError
+        with self.assertRaises(KeyError):
+            fs._prepare_outcomes(results[1], "non existing key")
+        
+        # classify function correct
+        def classify(data):
+            result = data['deceased population region 1']
+            classes =  np.zeros(result.shape[0])
+            classes[result[:, -1] > 1000000] = 1
+            return classes
+        
+        y, categorical = fs._prepare_outcomes(results[1], classify)
+        
+        self.assertTrue(categorical)
+        self.assertTrue(len(y.shape)==1)
+        
+        # neither string nor classify function --> TypeError
+        with self.assertRaises(TypeError):
+            fs._prepare_outcomes(results[1], 1)
+   
+   
+    def test_get_univariate_feature_scores(self):
+        fn = r'../data/1000 flu cases no policy.tar.gz'
+        results = load_results(fn)
+        
+        def classify(data):
+            #get the output for deceased population
+            result = data['deceased population region 1']
+            
+            #make an empty array of length equal to number of cases 
+            classes =  np.zeros(result.shape[0])
+            
+            #if deceased population is higher then 1.000.000 people, classify as 1 
+            classes[result[:, -1] > 1000000] = 1
+            
+            return classes
+        
+        scores = fs.get_univariate_feature_scores(results, classify)
+        self.assertEqual(len(scores), len(results[0].dtype.fields))
+   
+   
     def test_get_rf_feature_scores(self):
         fn = r'../data/1000 flu cases no policy.tar.gz'
         results = load_results(fn)
-
         
         def classify(data):
             #get the output for deceased population
@@ -87,9 +140,8 @@ class FeatureSelectionTestCase(unittest.TestCase):
     
 if __name__ == '__main__':
     ema_logging.log_to_stderr(ema_logging.INFO)   
-#     unittest.main()
+    unittest.main()
 
-
-    suite = unittest.TestSuite()
-    suite.addTest(FeatureSelectionTestCase("test_get_rf_feature_scores"))
-    unittest.TextTestRunner().run(suite)
+#     suite = unittest.TestSuite()
+#     suite.addTest(FeatureScoringTestCase("test_get_univariate_feature_scores"))
+#     unittest.TextTestRunner().run(suite)
