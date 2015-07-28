9a03eb47b9073fae38ce9831226c28185ac47398
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2015-07-28 16:37:24 +0200

    test_model filled in

--- src/test/test_expWorkbench/test_model.py
+++ src/test/test_expWorkbench/test_model.py
@@ -6,6 +6,7 @@ Created on Jul 28, 2015
 import unittest
 
 from expWorkbench import ModelStructureInterface
+from expWorkbench.ema_exceptions import EMAError
 
 class TestMSI(ModelStructureInterface):
     def model_init(self, policy, kwargs):
@@ -15,24 +16,61 @@ class TestMSI(ModelStructureInterface):
     def run_model(self, case):
         pass
 
-
-
 class Test(unittest.TestCase):
 
     def test_init(self):
-        pass
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        
+        self.assertEqual(model.name, model_name)
+        self.assertEqual(model.working_directory, wd)
+        self.assertRaises(EMAError, TestMSI, '', 'model name')
+        
 
     def test_model_init(self):
-        pass
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        policy = {'name':'test'}
+        model.model_init(policy, {})
+        
+        self.assertEqual(policy, model.policy)
+        self.assertEqual({}, model.kwargs)
     
     def test_run_model(self):
-        pass
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        model.run_model({})
     
     def test_retrieve_output(self):
-        pass
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        output = model.retrieve_output()
+        
+        self.assertEqual({}, output)
     
     def test_cleanup(self):
-        pass
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        model.cleanup()
+
+    def test_get_model_uncertainties(self):
+        model_name = 'modelname'
+        wd = '/test'
+        
+        model = TestMSI(wd, model_name)
+        uncs = model.get_model_uncertainties()
+        
+        self.assertEqual([], uncs)
 
 
 if __name__ == "__main__":
