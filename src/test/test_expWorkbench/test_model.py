5504f9ff4b86278c741be920195f5d980109c385
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2015-07-28 16:20:32 +0200

    test_model stub

--- src/test/test_expWorkbench/test_model.py
+++ src/test/test_expWorkbench/test_model.py
@@ -5,11 +5,33 @@ Created on Jul 28, 2015
 '''
 import unittest
 
+from expWorkbench import ModelStructureInterface
+
+class TestMSI(ModelStructureInterface):
+    def model_init(self, policy, kwargs):
+        self.policy = policy
+        self.kwargs = kwargs
+        
+    def run_model(self, case):
+        pass
+
+
 
 class Test(unittest.TestCase):
 
+    def test_init(self):
+        pass
 
-    def testName(self):
+    def test_model_init(self):
+        pass
+    
+    def test_run_model(self):
+        pass
+    
+    def test_retrieve_output(self):
+        pass
+    
+    def test_cleanup(self):
         pass
 
 
