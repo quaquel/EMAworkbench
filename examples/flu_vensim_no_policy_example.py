b219d05edbcdd40be4785880f028a8a360affcdc
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2016-07-20 10:09:45 +0200

    vensim updates
    vensim now can use the various updates

--- examples/flu_vensim_no_policy_example.py
+++ examples/flu_vensim_no_policy_example.py
@@ -10,72 +10,67 @@ is the same as used in fluExample
 '''
 
 from ema_workbench.em_framework import (ModelEnsemble, ParameterUncertainty, 
-                                        Outcome)
+                                        TimeSeriesOutcome)
 from ema_workbench.util import ema_logging, save_results
 
 from ema_workbench.connectors.vensim import VensimModelStructureInterface 
 
-class FluModel(VensimModelStructureInterface):
-    #base case model
-    model_file = r'\FLUvensimV1basecase.vpm'
-        
-    #outcomes
-    outcomes = [Outcome('deceased population region 1', time=True),
-                Outcome('infected fraction R1', time=True)]
- 
-    #Plain Parametric Uncertainties 
-    uncertainties = [
-        ParameterUncertainty((0, 0.5), 
-                             "additional seasonal immune population fraction R1"),
-        ParameterUncertainty((0, 0.5), 
-                             "additional seasonal immune population fraction R2"),
-        ParameterUncertainty((0.0001, 0.1), 
-                             "fatality ratio region 1"),
-        ParameterUncertainty((0.0001, 0.1), 
-                             "fatality rate region 2"),
-        ParameterUncertainty((0, 0.5), 
-                             "initial immune fraction of the population of region 1"),
-        ParameterUncertainty((0, 0.5), 
-                             "initial immune fraction of the population of region 2"),
-        ParameterUncertainty((0, 0.9), 
-                             "normal interregional contact rate"),
-        ParameterUncertainty((0, 0.5), 
-                             "permanent immune population fraction R1"),
-        ParameterUncertainty((0, 0.5), 
-                             "permanent immune population fraction R2"),
-        ParameterUncertainty((0.1, 0.75), 
-                             "recovery time region 1"),
-        ParameterUncertainty((0.1, 0.75), 
-                             "recovery time region 2"),
-        ParameterUncertainty((0.5,2), 
-                             "susceptible to immune population delay time region 1"),
-        ParameterUncertainty((0.5,2), 
-                             "susceptible to immune population delay time region 2"),
-        ParameterUncertainty((0.01, 5), 
-                             "root contact rate region 1"),
-        ParameterUncertainty((0.01, 5), 
-                             "root contact ratio region 2"),
-        ParameterUncertainty((0, 0.15), 
-                             "infection ratio region 1"),
-        ParameterUncertainty((0, 0.15), 
-                             "infection rate region 2"),
-        ParameterUncertainty((10, 100), 
-                             "normal contact rate region 1"),
-        ParameterUncertainty((10, 200), 
-                             "normal contact rate region 2")]
-                         
-        
 if __name__ == "__main__":
     ema_logging.log_to_stderr(ema_logging.INFO)
-        
-    model = FluModel(r'./models/flu', "fluCase")
+
+    model = VensimModelStructureInterface("fluCase", wd=r'./models/flu',
+                                          model_file = r'\FLUvensimV1basecase.vpm')
+            
+    #outcomes
+    model.outcomes = [TimeSeriesOutcome('deceased population region 1'),
+                      TimeSeriesOutcome('infected fraction R1')]
+    
+    #Plain Parametric Uncertainties 
+    model.uncertainties = [
+    ParameterUncertainty((0, 0.5), 
+                         "additional seasonal immune population fraction R1"),
+    ParameterUncertainty((0, 0.5), 
+                         "additional seasonal immune population fraction R2"),
+    ParameterUncertainty((0.0001, 0.1), 
+                         "fatality ratio region 1"),
+    ParameterUncertainty((0.0001, 0.1), 
+                         "fatality rate region 2"),
+    ParameterUncertainty((0, 0.5), 
+                         "initial immune fraction of the population of region 1"),
+    ParameterUncertainty((0, 0.5), 
+                         "initial immune fraction of the population of region 2"),
+    ParameterUncertainty((0, 0.9), 
+                         "normal interregional contact rate"),
+    ParameterUncertainty((0, 0.5), 
+                         "permanent immune population fraction R1"),
+    ParameterUncertainty((0, 0.5), 
+                         "permanent immune population fraction R2"),
+    ParameterUncertainty((0.1, 0.75), 
+                         "recovery time region 1"),
+    ParameterUncertainty((0.1, 0.75), 
+                         "recovery time region 2"),
+    ParameterUncertainty((0.5,2), 
+                         "susceptible to immune population delay time region 1"),
+    ParameterUncertainty((0.5,2), 
+                         "susceptible to immune population delay time region 2"),
+    ParameterUncertainty((0.01, 5), 
+                         "root contact rate region 1"),
+    ParameterUncertainty((0.01, 5), 
+                         "root contact ratio region 2"),
+    ParameterUncertainty((0, 0.15), 
+                         "infection ratio region 1"),
+    ParameterUncertainty((0, 0.15), 
+                         "infection rate region 2"),
+    ParameterUncertainty((10, 100), 
+                         "normal contact rate region 1"),
+    ParameterUncertainty((10, 200), 
+                         "normal contact rate region 2")]
+                     
+
     ensemble = ModelEnsemble()
     ensemble.model_structure = model
     
-    ensemble.parallel = True #turn on parallel processing
+#     ensemble.parallel = True #turn on parallel processing
 
-    nr_experiments = 1000
+    nr_experiments = 100
     results = ensemble.perform_experiments(nr_experiments)
-    
-    fh =  r'./data/{} flu cases no policy.tar.gz'.format(nr_experiments)
-    save_results(results, fh)
