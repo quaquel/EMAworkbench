6bc780c2e3ee10eb9f080e25993df3bf0f4703c0
James Houghton <James.p.houghton@gmail.com>
2016-07-21 15:11:44 +0200

    Adding PySD Connector

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-#from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
     name='ema_workbench',
-    version='0.1dev',
+    version='0.1.1dev',
     packages=find_packages(exclude=['ez_setup']),
     license='GNU General Public License',
-    long_description=open('README.md').read(),
+    long_description='Exploratory Modeling and Analysis',
 )
