bebbd9f7956a11c87ec412ba0646d5fc1f17beec
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2017-02-17 10:29:26 +0100

    small import change

--- src/ema_workbench/__init__.py
+++ src/ema_workbench/__init__.py
@@ -3,7 +3,7 @@ from . import analysis
 from . import em_framework
 from .em_framework import (Model, RealParameter, CategoricalParameter, 
                            IntegerParameter, perform_experiments, ScalarOutcome, 
-                           TimeSeriesOutcome, Constant)
+                           TimeSeriesOutcome, Constant, Scenario, Policy)
 
 from . import util
 from .util import save_results, load_results, ema_logging
