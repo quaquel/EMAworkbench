717fd4fd75a6a1e2864a38cbccd92ebeb54ed20d
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-04-10 20:10:09 +0200

    update of netlogo example to netlogo6

--- ema_workbench/examples/netlogo_example.py
+++ ema_workbench/examples/netlogo_example.py
@@ -1,15 +1,22 @@
 '''
-Created on 20 mrt. 2013
 
-@author: localadmin
+This example is a proof of principle for how NetLogo models can be
+controlled using pyNetLogo and the ema_workbench. Note that this
+example uses the NetLogo 6 version of the predator prey model that 
+comes with NetLogo. If you are using NetLogo 5, replace the model file
+with the one that comes with NetLogo.
+
 '''
 from __future__ import unicode_literals, absolute_import
 
 from ema_workbench.connectors.netlogo import NetLogoModel
 
-from ema_workbench import (RealParameter, ema_logging,
-                           perform_experiments,TimeSeriesOutcome)
-from ema_workbench.em_framework.evaluators import MultiprocessingEvaluator
+from ema_workbench import (RealParameter, ema_logging, 
+                           TimeSeriesOutcome, MultiprocessingEvaluator)
+
+# Created on 20 mrt. 2013
+#
+# .. codeauthor::  jhkwakkel
 
 
 if __name__ == '__main__':
@@ -23,10 +30,10 @@ if __name__ == '__main__':
     model.replications = 10
     
     model.uncertainties = [RealParameter("grass-regrowth-time", 1, 99),
-                           RealParameter("initial-number-sheep", 1, 200),
-                           RealParameter("initial-number-wolves", 1, 200),
-                           RealParameter("sheep-reproduce", 1, 20),
-                           RealParameter("wolf-reproduce", 1, 20),
+                           RealParameter("initial-number-sheep", 50, 100),
+                           RealParameter("initial-number-wolves", 50, 100),
+                           RealParameter("sheep-reproduce", 5, 10),
+                           RealParameter("wolf-reproduce", 5, 10),
                      ]
     
     model.outcomes = [TimeSeriesOutcome('sheep'),
@@ -37,4 +44,4 @@ if __name__ == '__main__':
     n = 10
     
     with MultiprocessingEvaluator(model) as evaluator:
-        results = perform_experiments(model, n, evaluator=evaluator)
+        results = evaluator.perform_experiments(n)
