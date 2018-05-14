47786b4b3c3ed5ecf47cae406280d2f52cf34a88
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-05-14 17:23:47 +0200

    checkpunt

---
+++ docs/source/indepth_tutorial/.ipynb_checkpoints/dps_lake_model-checkpoint.py
@@ -0,0 +1,87 @@
+from __future__ import division # python2 
+import math
+import numpy as np
+from scipy.optimize import brentq
+
+def get_antropogenic_release(xt, c1, c2, r1, r2, w1):
+    '''
+    
+    Parameters
+    ----------
+    xt : float
+         polution in lake at time t
+    c1 : float
+         center rbf 1
+    c2 : float
+         center rbf 2
+    r1 : float
+         ratius rbf 1
+    r2 : float
+         ratius rbf 2
+    w1 : float
+         weight of rbf 1
+         
+    note:: w2 = 1 - w1
+    
+    '''
+    
+    rule = w1*(abs(xt-c1/r1))**3+(1-w1)*(abs(xt-c2/r2))**3
+    at = min(max(rule, 0.01), 0.1)
+    return at
+
+
+def lake_model(b=0.42, q=2.0, mean=0.02, stdev=0.001, alpha=0.4,     
+               delta=0.98, c1=0.25, c2=0.25, r1=0.5, r2=0.5,
+               w1=0.5, nsamples=100, steps=100, seed=None):    
+    '''runs the lake model for 1 stochastic realisation using specified 
+    random seed.
+        
+    Parameters
+    ----------
+    b : float
+        decay rate for P in lake (0.42 = irreversible)
+    q : float
+        recycling exponent
+    mean : float
+            mean of natural inflows
+    stdev : float
+            standard deviation of natural inflows
+    alpha : float
+            utility from pollution
+    delta : float
+            future utility discount rate
+    c1 : float
+    c2 : float
+    r1 : float
+    r2 : float
+    w1 : float
+    steps : int
+            the number of time steps (e.g., days)
+    seed : int, optional
+           seed for the random number generator
+    
+    '''
+    np.random.seed(seed)
+    
+    Pcrit = brentq(lambda x: x**q/(1+x**q) - b*x, 0.01, 1.5)
+    X = np.zeros((steps,))
+    decisions = np.zeros((steps,))
+
+    X[0] = 0.0
+
+    natural_inflows = np.random.lognormal(
+            math.log(mean**2 / math.sqrt(stdev**2 + mean**2)),
+            math.sqrt(math.log(1.0 + stdev**2 / mean**2)),
+            size=steps)
+
+    for t in range(steps-1):
+        decisions[t] = get_antropogenic_release(X[t], c1, c2, r1, r2, w1)
+        X[t+1] = (1-b)*X[t] + X[t]**q/(1+X[t]**q) + decisions[t] + natural_inflows[t]
+
+    reliability = np.sum(X < Pcrit)/steps
+    utility = np.sum(alpha*decisions*np.power(delta,np.arange(steps)))
+    
+    # note that I have slightly changed this formulation to retain
+    # consistency with the equations in the papers
+    inertia = np.sum(np.abs(np.diff(decisions)) < 0.01)/(steps-1)
+    return X, utility, inertia, reliability
