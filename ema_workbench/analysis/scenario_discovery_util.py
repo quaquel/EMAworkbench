0fbeff757a3aaa4b1c7d324cc266dbe494da3ee9
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-10-10 16:05:06 +0200

    scenario_discovery_util passes all tests

--- ema_workbench/analysis/scenario_discovery_util.py
+++ ema_workbench/analysis/scenario_discovery_util.py
@@ -54,7 +54,7 @@ def _get_sorted_box_lims(boxes, box_init):
     # in one or more boxes
     uncs = set()
     for box in boxes:
-        us = _determine_restricted_dims(box, box_init).tolist()
+        us = _determine_restricted_dims(box, box_init)
         uncs = uncs.union(us)
     uncs = np.asarray(list(uncs))
 
@@ -77,32 +77,22 @@ def _make_box(x):
 
     Parameters
     ----------
-    x : structured numpy array
+    x : DataFrame
+    
+    Returns
+    -------
+    DataFrame
 
 
     '''
 
-    box = np.zeros((2, ), x.dtype)
-
-    names = recfunctions.get_names(x.dtype)
-
-    for name in names:
-        dtype = x.dtype.fields.get(name)[0]
-        mask = np.ma.getmaskarray(x[name])
-        values = x[name][mask == False]
-
-        if dtype == 'object':
-            try:
-                values = set(values)
-            except TypeError as e:
-                ema_logging.warning("{} has unhashable values".format(name))
-                raise e
-            else:
-                box[name][:] = values
+    def limits(x):
+        if (x.dtype == int) or (x.dtype == float):
+            return pd.Series([x.min(), x.max()])
         else:
-            box[name][0] = np.min(values, axis=0)
-            box[name][1] = np.max(values, axis=0)
-    return box
+            return pd.Series([set(x), set(x)])
+
+    return x.apply(limits)
 
 
 def _normalize(box_lim, box_init, uncertainties):
@@ -114,8 +104,8 @@ def _normalize(box_lim, box_init, uncertainties):
 
     Parameters
     ----------
-    box_lim : a numpy structured array.
-    box_init :  a numpy structured array.
+    box_lim : DataFrame
+    box_init :  DataFrame
     uncertainties : list of strings
                     valid names of columns that exist in both structured 
                     arrays.
@@ -123,7 +113,8 @@ def _normalize(box_lim, box_init, uncertainties):
     Returns
     -------
     ndarray
-        a numpy array of the shape (2, len(uncertainties) with the box limits.
+        a numpy array of the shape (2, len(uncertainties) with the 
+        normalized box limits.
 
 
     '''
@@ -132,41 +123,38 @@ def _normalize(box_lim, box_init, uncertainties):
     norm_box_lim = np.zeros((len(uncertainties), box_lim.shape[0]))
 
     for i, u in enumerate(uncertainties):
-        dtype = box_lim.dtype.fields[u][0]
+        dtype = box_lim[u].dtype
         if dtype == np.dtype(object):
-            nu = len(box_lim[u][0]) / len(box_init[u][0])
+            nu = len(box_lim.loc[0, u]) / len(box_init.loc[0, u])
             nl = 0
         else:
-            lower, upper = box_lim[u]
-            dif = (box_init[u][1]-box_init[u][0])
+            lower, upper = box_lim.loc[:, u]
+            dif = (box_init.loc[1, u]-box_init.loc[0, u])
             a = 1/dif
-            b = -1 * box_init[u][0] / dif
+            b = -1 * box_init.loc[0, u] / dif
             nl = a * lower + b
             nu = a * upper + b
         norm_box_lim[i, :] = nl, nu
     return norm_box_lim
 
 
-def _determine_restricted_dims(box_lims, box_init):
-    '''
-
-    determine which dimensions of the given box are restricted compared 
-    to compared to the initial box that contains all the data
-
+def _determine_restricted_dims(box_limits, box_init):
+    '''returns a list of dimensions that is restricted
+    
     Parameters
     ----------
-    box_lims : structured numpy array
-               a specific box limit
-    box_init : structured numpy array
-               the initial box containing all data points
-
+    box_limits : pd.DataFrame
+    box_init : pd.DataFrame 
+    
+    Returns
+    -------
+    list of str
+    
     '''
-
-    logical = _compare(box_init, box_lims)
-    u = np.asarray(recfunctions.get_names(box_lims.dtype),
-                   dtype=object)
-    dims = u[logical == False]
-    return dims
+    
+    restricted_dims = [column for column in box_init.columns if not
+           np.all(box_init[column].values == box_limits[column].values)]
+    return restricted_dims
 
 
 def _determine_nr_restricted_dims(box_lims, box_init):
@@ -181,10 +169,15 @@ def _determine_nr_restricted_dims(box_lims, box_init):
                a specific box limit
     box_init : structured numpy array
                the initial box containing all data points
+               
+               
+    Returns
+    -------
+    int
 
     '''
 
-    return _determine_restricted_dims(box_lims, box_init).shape[0]
+    return len(_determine_restricted_dims(box_lims, box_init))
 
 
 def _compare(a, b):
@@ -217,8 +210,8 @@ def _setup_figure(uncs):
                                  facecolor="#C0C0C0",
                                  edgecolor="#C0C0C0")
     ax.add_patch(rect)
-    ax.set_xlim(xmin=-0.2, xmax=1.2)
-    ax.set_ylim(ymin=-0.5, ymax=nr_unc-0.5)
+    ax.set_xlim(left=-0.2, right=1.2)
+    ax.set_ylim(top=-0.5, bottom=nr_unc-0.5)
     ax.yaxis.set_ticks([y for y in range(nr_unc)])
     ax.xaxis.set_ticks([0, 0.25, 0.5, 0.75, 1])
     ax.set_yticklabels(uncs[::-1])
@@ -228,54 +221,40 @@ def _setup_figure(uncs):
 def _in_box(x, boxlim):
     '''
 
-    returns the indices of the data points that are within the 
-    box_lims.
-
+    returns the a boolean index indicated which data points are inside
+    and which are outside of the given box_lims
+    
     Parameters
     ----------
-    x : numpy structured array
-    boxlim : numpy structured array
-
-
+    x : pd.DataFrame
+    boxlim : pd.DataFrame 
+    
     Returns
     -------
     ndarray
-        valid numpy indices on x
+        boolean 1D array
 
     '''
     logical = np.ones(x.shape[0], dtype=np.bool)
 
-    dims = recfunctions.get_names(boxlim.dtype)
+    for column, values in x.select_dtypes(np.number).iteritems():
+        logical = logical & (boxlim.loc[0, column] <= values) &\
+                            (values <= boxlim.loc[1, column])
 
-    for name in dims:
-        value = x.dtype.fields.get(name)[0]
+    for column, values in x.select_dtypes(exclude=np.number).iteritems():
+        #     for column, values in x.select_dtypes(pd.Categorical).iteritems():
+        # Note:: if we force all nno numbered to be categorical, 
+        # either in callback, or at init of CART and PRIM, 
+        # this code could be cleaner because the astype could be removed  
+        entries = boxlim.loc[0, column]
+        not_present = set(values.astype('category').cat.categories.values) - entries
 
-        if value == 'object':
-            entries = boxlim[name][0]
-            l = np.ones((x.shape[0], len(entries)), dtype=np.bool)
-            for i, entry in enumerate(entries):
-                if type(list(entries)[0]) not in (str, float, int):
-                    bools = []
-                    for element in list(x[name]):
-                        if element == entry:
-                            bools.append(True)
-                        else:
-                            bools.append(False)
-                    l[:, i] = np.asarray(bools, dtype=bool)
-                else:
-                    l[:, i] = x[name] == entry
-            l = np.any(l, axis=1)
-            logical = logical & l
-        else:
-            logical = logical & (boxlim[name][0] <= x[name]) &\
-                (x[name] <= boxlim[name][1])
-
-    indices = np.where(logical == True)
-
-    assert len(indices) == 1
-    indices = indices[0]
-
-    return indices
+        if not_present:
+            # what other options do we have here....
+#             l = pd.isnull(x[column].astype('category').cat.remove_categories(list(not_present)))
+            l = pd.isnull(x[column].astype('category').cat.remove_categories(list(entries)))
+            logical = l & logical
+    return logical
 
 
 def _calculate_quasip(x, y, box, Hbox, Tbox):
@@ -340,8 +319,8 @@ class OutputFormatterMixin(object):
         nr_boxes = len(boxes)
         dtype = float
         index = ["box {}".format(i+1) for i in range(nr_boxes)]
-        for value in box_lims[0].dtype.fields.values():
-            if value[0] == object:
+        for value in box_lims[0].dtypes:
+            if value == object:
                 dtype = object
                 break
 
@@ -351,12 +330,12 @@ class OutputFormatterMixin(object):
                                 index=uncs,
                                 dtype=dtype,
                                 columns=columns)
-
+        
+        # TODO should be possible to make more efficient
         for i, box in enumerate(box_lims):
             for unc in uncs:
-                values = box[unc][:]
-                values = pd.Series(values,
-                                   index=['min', 'max'])
+                values = box.loc[:, unc]
+                values = values.rename({0:'min', 1:'max'})
                 df_boxes.ix[unc][index[i]] = values
         return df_boxes
 
@@ -386,18 +365,19 @@ class OutputFormatterMixin(object):
         # plot.
         norm_box_lims = [_normalize(box_lim, box_init, uncs) for
                          box_lim in box_lims[0:-1]]
-        colors = itertools.cycle(COLOR_LIST)
-
+        
         if together:
             fig, ax = _setup_figure(uncs)
-            color = next(colors)
 
             for i, u in enumerate(uncs):
+                colors = itertools.cycle(COLOR_LIST)
                 # we want to have the most restricted dimension
                 # at the top of the figure
+                
                 xi = len(uncs) - i - 1
 
                 for j, norm_box_lim in enumerate(norm_box_lims):
+                    color = next(colors)
                     self._plot_unc(box_init, xi, i, j, norm_box_lim,
                                    box_lims[j], u, ax, color)
 
@@ -405,20 +385,26 @@ class OutputFormatterMixin(object):
             return fig
         else:
             figs = []
-            color = next(colors)
+            colors = itertools.cycle(COLOR_LIST)
+            
+            
             for j, norm_box_lim in enumerate(norm_box_lims):
                 fig, ax = _setup_figure(uncs)
+                ax.set_title('box {}'.format(j))
+                color = next(colors)
+                
                 figs.append(fig)
                 for i, u in enumerate(uncs):
                     xi = len(uncs) - i - 1
-                    self._plot_unc(box_init, xi, i, j, norm_box_lim,
+                    self._plot_unc(box_init, xi, i, 0, norm_box_lim,
                                    box_lims[j], u, ax, color)
 
                 plt.tight_layout()
             return figs
 
     @staticmethod
-    def _plot_unc(box_init, xi, i, j, norm_box_lim, box_lim, u, ax):
+    def _plot_unc(box_init, xi, i, j, norm_box_lim, box_lim, u, ax,
+                  color):
         '''
 
         Parameters:
@@ -440,7 +426,6 @@ class OutputFormatterMixin(object):
 
         y = xi-j*0.1
 
-        color = COLOR_LIST[0]
         if dtype == object:
             elements = sorted(list(box_init[u][0]))
             max_value = (len(elements)-1)
