55267393a887624eee54c159552fadf9696d8343
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-10-10 15:07:42 +0200

    additional cart example and updated associated data

---
+++ ema_workbench/examples/cart_wcm_example.py
@@ -0,0 +1,36 @@
+'''
+Created on May 26, 2015
+
+@author: jhkwakkel
+'''
+from __future__ import (print_function)
+
+import matplotlib.pyplot as plt
+
+import ema_workbench.analysis.cart as cart
+from ema_workbench import ema_logging, load_results, save_results
+
+ema_logging.log_to_stderr(level=ema_logging.INFO)
+
+default_flow = 2.178849944502783e7
+
+# load data
+fn = './data/5000 runs WCM.tar.gz'
+results = load_results(fn)
+x, outcomes = results
+  
+ooi = 'throughput Rotterdam'
+outcome = outcomes[ooi]/default_flow
+y = outcome < 1
+  
+cart_alg = cart.CART(x,y)
+cart_alg.build_tree()
+   
+# print cart to std_out
+print(cart_alg.stats_to_dataframe())
+print(cart_alg.boxes_to_dataframe())
+   
+# visualize
+cart_alg.display_boxes(together=False)
+cart_alg.show_tree()
+plt.show()
