e0e47c7cedfd5cb134cfc2ab1af3233642c0e825
Jeffrey Newman <jeff@newman.me>
2018-12-18 16:25:19 -0600

    BooleanParameter

--- ema_workbench/em_framework/parameters.py
+++ ema_workbench/em_framework/parameters.py
@@ -17,7 +17,7 @@ from ema_workbench.em_framework.util import (NamedObject, Variable,
 #
 # .. codeauthor::jhkwakkel <j.h.kwakkel (at) tudelft (dot) nl>
 
-__all__ = ['Parameter', 'RealParameter', 'IntegerParameter',
+__all__ = ['Parameter', 'RealParameter', 'IntegerParameter', 'BooleanParameter',
            'CategoricalParameter', 'create_parameters',
            'experiment_generator']
 
@@ -315,6 +315,44 @@ class CategoricalParameter(IntegerParameter):
         return representation
 
 
+class BooleanParameter(IntegerParameter):
+    ''' boolean model input parameter
+
+    A BooleanParameter is similar to a CategoricalParameter, except
+    the category values can only be True or False.
+
+    Parameters
+    ----------
+    name : str
+    variable_name : str, or list of str
+
+    '''
+
+    def __init__(self, name, default=None, variable_name=None, pff=False):
+        lower_bound = 0
+        upper_bound = 1
+
+        super(BooleanParameter, self).__init__(
+            name, lower_bound,
+            upper_bound, resolution=None, default=default,
+            variable_name=variable_name, pff=pff)
+
+        self.categories = [False, True]
+        self.resolution = [0, 1]
+
+    def __repr__(self, *args, **kwargs):
+        template1 = 'BooleanParameter(\'{}\', default={})'
+        template2 = 'BooleanParameter(\'{}\', )'
+
+        if self.default:
+            representation = template1.format(self.name,
+                                              self.default)
+        else:
+            representation = template2.format(self.name, )
+
+        return representation
+
+
 class Policy(NamedDict):
     # TODO:: separate id and name
     # if name is not provided fall back on id
@@ -506,7 +544,9 @@ def create_parameters(uncertainties, **kwargs):
 
     parameter_map = {'int': IntegerParameter,
                      'real': RealParameter,
-                     'cat': CategoricalParameter}
+                     'cat': CategoricalParameter,
+                     'bool': BooleanParameter,
+                     }
 
     # check if names column is there
     if ('NAME' not in uncertainties) and ('name' not in uncertainties):
