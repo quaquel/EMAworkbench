3af3c5b17bfcdb263e50d7e7509dbda5283a5828
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2019-02-13 19:02:44 +0100

    temporary fix for segfault of netlogo in multiprocessing

--- ema_workbench/analysis/__init__.py
+++ ema_workbench/analysis/__init__.py
@@ -1,11 +1,13 @@
-from __future__ import absolute_import
 
-from .prim import Prim, run_constrained_prim, pca_preprocess, setup_prim
-from .cart import setup_cart, CART
-from .b_and_w_plotting import set_fig_to_bw
-from .plotting import lines, envelopes, kde_over_time, multiple_densities
-from .plotting_util import Density, PlotType
-from . import pairs_plotting
-from .feature_scoring import (get_ex_feature_scores, get_feature_scores_all,
-                        get_rf_feature_scores, get_univariate_feature_scores)
-from .scenario_discovery_util import RuleInductionType
+# importing anything from analysis segfaults java with netlogo on a mac
+# for now no clue why
+#
+# from .prim import Prim, run_constrained_prim, pca_preprocess, setup_prim
+# from .cart import setup_cart, CART
+# from .b_and_w_plotting import set_fig_to_bw
+# from .plotting import lines, envelopes, kde_over_time, multiple_densities
+# from .plotting_util import Density, PlotType
+# from . import pairs_plotting
+# from .feature_scoring import (get_ex_feature_scores, get_feature_scores_all,
+#                         get_rf_feature_scores, get_univariate_feature_scores)
+# from .scenario_discovery_util import RuleInductionType
