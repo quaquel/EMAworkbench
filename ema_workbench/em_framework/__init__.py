9380902c364a7c517d6fb93848c3d2581bab1de1
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2019-03-04 08:51:54 +0100

    minor modification in imports and updated todo

--- ema_workbench/em_framework/__init__.py
+++ ema_workbench/em_framework/__init__.py
@@ -15,7 +15,7 @@ __all__ = ["ema_parallel", "parameters"
 
 from .outcomes import (ScalarOutcome, TimeSeriesOutcome, Constraint,
                        ArrayOutcome)
-from .model import Model, FileModel, ReplicatorModel
+from .model import Model, FileModel, ReplicatorModel, Replicator, SingleReplication
 
 from .parameters import (RealParameter, IntegerParameter, CategoricalParameter,
                          BooleanParameter, Scenario, Policy, Constant,
