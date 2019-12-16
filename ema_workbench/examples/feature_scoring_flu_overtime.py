75588482105e3878790a269b377d3fbbcb3cff88
quaquel <j.h.kwakkel@tudelft.nl>
2019-12-16 10:55:06 +0100

    Update feature_scoring_flu_overtime.py

--- ema_workbench/examples/feature_scoring_flu_overtime.py
+++ ema_workbench/examples/feature_scoring_flu_overtime.py
@@ -5,6 +5,7 @@ Created on 30 Oct 2018
 '''
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 from ema_workbench import ema_logging, load_results
 from ema_workbench.analysis import (get_ex_feature_scores,
@@ -14,32 +15,31 @@ ema_logging.log_to_stderr(level=ema_logging.INFO)
 
 # load data
 fn = r'./data/1000 flu cases no policy.tar.gz'
-x, outcomes = load_results(fn)
 
+x, outcomes = load_results(fn)
 x = x.drop(['model', 'policy'], axis=1)
+
 y = outcomes['deceased population region 1']
 
 
-#
-# 'infected fraction R1'
-
 all_scores = []
-for i in range(0, y.shape[1], 2):
+
+# we only want to show those uncertainties that are in the top 5
+# most sensitive parameters at any time step
+top_5 = set()
+for i in range(2, y.shape[1], 8):
     data = y[:, i]
     scores = get_ex_feature_scores(x, data,
                                    mode=RuleInductionType.REGRESSION)[0]
-
+    # add the top five for this time step to the set of top5s
+    top_5 |= set(scores.nlargest(5, 1).index.values)   
+    scores = scores.rename(columns={1:outcomes['TIME'][0, i]})
     all_scores.append(scores)
+    
 all_scores = pd.concat(all_scores, axis=1, sort=False)
-all_scores[all_scores < 0.075] = 0  # cleans up results
-normalized = all_scores.divide(all_scores.sum(axis=1), axis=0)
-normalized = all_scores.divide(all_scores.sum(axis=1), axis=0)
+all_scores = all_scores.loc[top_5, :]
 
-labels = normalized.index.values
-print(labels)
-y = normalized.values
 
 fig, ax = plt.subplots()
-ax.stackplot(range(len(labels)), y.T, labels=labels)
-# ax.legend()
+sns.heatmap(all_scores, ax=ax, cmap='viridis')
 plt.show()
