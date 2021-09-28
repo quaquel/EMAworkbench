4c19d3fe06fbf2ab5d0ad7720a11b7fc09adb824
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2021-09-28 10:12:07 +0200

    Update test_ema_ipyparallel.py

--- test/test_em_framework/test_ema_ipyparallel.py
+++ test/test_em_framework/test_ema_ipyparallel.py
@@ -4,9 +4,6 @@ test code for ema_ipyparallel. The setup and teardown of the cluster is
 taken from the ipyparallel test code with some minor adaptations
 .. codeauthor:: jhkwakkel <j.h.kwakkel (at) tudelft (dot) nl>
 '''
-from __future__ import (unicode_literals, division, print_function, 
-                        absolute_import)
-
 import logging
 
 import unittest.mock as mock
