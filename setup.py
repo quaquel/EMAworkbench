80a1ccd0ad2685e43d5d489709b46c1af642a2af
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2022-04-18 15:06:42 +0200

    reducing install file size

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import re
 
 from setuptools import setup
 
+
 def read(path, encoding='utf-8'):
     path = os.path.join(os.path.dirname(__file__), path)
     with io.open(path, encoding=encoding) as fp:
@@ -24,13 +25,13 @@ def version(path):
 
 def package_files(root, package_data_dir):
     paths = []
-    
+
     directory = os.path.join(root, package_data_dir)
-    
+
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
             full_path = os.path.join(path, filename)
-            
+
             paths.append(os.path.relpath(full_path, root))
     return paths
 
@@ -43,25 +44,24 @@ example_model_files = package_files('ema_workbench/examples', 'models')
 example_model_files = [''.join(['examples/', entry]) for entry in
                        example_model_files]
 
-java_files = package_files('ema_workbench/connectors', 'java')
-java_files = [''.join(['connectors/', entry]) for entry in java_files]
-
 name = 'ema_workbench'
 pjoin = os.path.join
 here = os.path.abspath(os.path.dirname(__file__))
 pkg_root = pjoin(here, name)
 
 packages = []
-for d, _, _ in os.walk(pjoin(here, name)):
+for d, _, _ in os.walk(pkg_root):
     if os.path.exists(pjoin(d, '__init__.py')):
-        packages.append(d[len(here)+1:].replace(os.path.sep, '.'))
+        packages.append(d[len(here) + 1:].replace(os.path.sep, '.'))
 
 VERSION = version('ema_workbench/__init__.py')
 LONG_DESCRIPTION = """Project Documentation: 
 https://emaworkbench.readthedocs.io/"""
 EXAMPLE_DATA = example_data_files + example_model_files
-JAVA = java_files
+
 PACKAGES = packages
+
+print(PACKAGES)
 
 setup(
     name='ema_workbench',
@@ -71,7 +71,7 @@ setup(
     author='Jan Kwakkel',
     author_email='j.h.kwakkel@tudelft.nl',
     packages=PACKAGES,
-    package_data={'ema_workbench': EXAMPLE_DATA+JAVA},
+    package_data={'ema_workbench': EXAMPLE_DATA},
     url='https://github.com/quaquel/EMAworkbench',
     license='BSD 3-Clause',
     platforms="Linux, Mac OS X, Windows",
