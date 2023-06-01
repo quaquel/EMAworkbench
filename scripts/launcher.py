1becad49732529a9e4178eee39a08e65cc5dffbd
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-06-01 10:29:08 +0200

    scripts: Merge model and launcher, ensure they work when cores < jobs
    Make sure that the scripts also work when the number of cores (in this case 10) is smaller than the number of jobs (20). In the EMAworkbench, when testing a million experiments, there won't be always a million cores available.

--- scripts/launcher.py
+++
@@ -1,7 +0,0 @@
-# launcher.py
-from mpi4py.futures import MPIPoolExecutor
-import my_model
-
-if __name__ == '__main__':
-    with MPIPoolExecutor() as executor:
-        executor.submit(my_model)
--- scripts/my_model.py
+++ scripts/my_model.py
@@ -9,11 +9,18 @@ def my_model(data):
     x, y = data
     return x**2 + y**2
 
-if __name__ == '__main__':
+def main():
     # Define the input data
     data = np.arange(20).reshape(-1, 2)  # Let's say we have 10 sets of (x, y) pairs
 
-    with MPIPoolExecutor() as executor:
+    # The size of COMM_WORLD gives us the number of processes across all nodes
+    n_cores = MPI.COMM_WORLD.Get_size()
+    n_jobs = len(data)  # Your actual number of jobs
+
+    if n_jobs > n_cores:
+        n_jobs = n_cores
+
+    with MPIPoolExecutor(max_workers=n_jobs) as executor:
         # Apply the model to the data
         results = list(executor.map(my_model, data))
 
@@ -22,3 +29,8 @@ if __name__ == '__main__':
 
     with open('py_test.pickle', 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+if __name__ == '__main__':
+    with MPIPoolExecutor() as executor:
+        future = executor.submit(main)
+        future.result()
