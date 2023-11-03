90908518a77c3c03d86e1d78c150f2e6b39eb427
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2023-11-03 16:30:47 +0100

    Speed up of plot_discrete_cdfs by 2 orders of magnitude (#306)
    * removal of scatter plot in plot_discrete_cdf for 2 orders of magnitude speedup
    * add a regional sa example
    * bug fix in plot_cdfs and speed up in plot_discrete_cdf

---
+++ ema_workbench/examples/regional_sa_flu.py
@@ -0,0 +1,18 @@
+""" A simple example of performing regional sensitivity analysis
+
+
+"""
+import matplotlib.pyplot as plt
+
+from ema_workbench.analysis import regional_sa
+from ema_workbench import ema_logging, load_results
+
+fn = "./data/1000 flu cases with policies.tar.gz"
+results = load_results(fn)
+x, outcomes = results
+
+y = outcomes["deceased population region 1"][:, -1] > 1000000
+
+fig = regional_sa.plot_cdfs(x, y)
+
+plt.show()
