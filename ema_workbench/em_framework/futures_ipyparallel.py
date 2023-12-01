222d4f5e1de7183940820774909d831d9a363084
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2023-12-01 18:58:09 +0100

    another attempt

--- ema_workbench/em_framework/futures_ipyparallel.py
+++ ema_workbench/em_framework/futures_ipyparallel.py
@@ -23,8 +23,6 @@ try:
     from zmq.eventloop import ioloop, zmqstream
 except (ImportError, ModuleNotFoundError):
     warnings.warn("ipyparallel not installed - IpyparalleEvaluator not available")
-    LoggingConfigurable = object
-    List = Instance = object
 
 from . import experiment_runner
 from .futures_util import setup_working_directories
