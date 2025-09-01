5ee003d9cedac88af980b965518bc1b8749507fb
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-09-01 09:54:17 +0200

    ruff fixes __init__ analysis package

--- ema_workbench/analysis/__init__.py
+++ ema_workbench/analysis/__init__.py
@@ -1,6 +1,32 @@
+"""Main namespace for analysis package."""
+
 # importing anything from analysis segfaults java with netlogo on a mac
 # for now no clue why
 #
+
+__all__ = [
+    "CART",
+    "Density",
+    "DiagKind",
+    "Logit",
+    "PlotType",
+    "Prim",
+    "RuleInductionType",
+    "envelopes",
+    "get_ex_feature_scores",
+    "get_feature_scores_all",
+    "get_rf_feature_scores",
+    "get_univariate_feature_scores",
+    "kde_over_time",
+    "lines",
+    "multiple_densities",
+    "pairs_plotting",
+    "pca_preprocess",
+    "run_constrained_prim",
+    "set_fig_to_bw",
+    "setup_cart",
+    "setup_prim"
+]
 
 from . import pairs_plotting
 from .b_and_w_plotting import set_fig_to_bw
