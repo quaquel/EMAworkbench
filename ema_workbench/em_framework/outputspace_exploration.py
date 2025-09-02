d8f5c0c156946879916046e395354d40c6c761ad
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-09-02 15:35:12 +0200

    ruff fixes for outputspace exploration

--- ema_workbench/em_framework/outputspace_exploration.py
+++ ema_workbench/em_framework/outputspace_exploration.py
@@ -44,7 +44,7 @@ __all__ = ["AutoAdaptiveOutputSpaceExploration", "OutputSpaceExploration"]
 
 
 class Novelty(Dominance):
-    """Compares to solutions based on their novelty
+    """Compares to solutions based on their novelty.
 
     Parameters
     ----------
@@ -82,7 +82,7 @@ class Novelty(Dominance):
 
 
 class HitBox(Archive):
-    """Hit Box archive
+    """Hit Box archive.
 
     Parameters:
     ----------
@@ -100,6 +100,7 @@ class HitBox(Archive):
     """
 
     def __init__(self, grid_spec):
+        """Init."""
         super().__init__(None)
         self.archive = {}
         self.centroids = {}
@@ -109,6 +110,7 @@ class HitBox(Archive):
         self.overall_novelty = 0
 
     def add(self, solution):
+        """Add a solution to the archive."""
         key = get_index_for_solution(solution, self.grid_spec)
 
         try:
@@ -143,20 +145,36 @@ class HitBox(Archive):
         return True
 
     def get_novelty_score(self, solution):
+        """Return the novelty score of the solution."""
+        # fixme make property
         key = get_index_for_solution(solution, self.grid_spec)
         return 1 / self.grid_counter[key]
 
 
 class OutputSpaceExplorationAlgorithm(AbstractGeneticAlgorithm):
+    """Algorithm for novelty search based exploration of output space.
+
+    Parameters
+    ----------
+    problem: the optimization problem
+    grid_spec: specification of the grid used for novelty search
+    population_size: population size for the algorithm
+    generator: generator of candidate solutions
+    variator: the evolutionary variator to create new solutions.
+
+
+
+    """
     def __init__(
         self,
         problem,
         grid_spec=None,
         population_size=100,
-        generator=RandomGenerator(),
+        generator=RandomGenerator(), # noqa: B008
         variator=None,
         **kwargs,
     ):
+        """Init."""
         if problem.nobjs != len(grid_spec):
             raise EMAError(
                 f"The number of items in grid_spec ({len(grid_spec)})"
@@ -173,6 +191,7 @@ class OutputSpaceExplorationAlgorithm(AbstractGeneticAlgorithm):
         self.comparator = Novelty(self)
 
     def step(self):
+        """A single step of the algorithm."""
         if self.nfe == 0:
             self.initialize()
         else:
@@ -181,6 +200,7 @@ class OutputSpaceExplorationAlgorithm(AbstractGeneticAlgorithm):
         self.result = self.archive
 
     def initialize(self):
+        """Initialize the algorithm."""
         super().initialize()
 
         if self.archive is not None:
@@ -190,6 +210,7 @@ class OutputSpaceExplorationAlgorithm(AbstractGeneticAlgorithm):
             self.variator = PlatypusConfig.default_variator(self.problem)
 
     def iterate(self):
+        """A signle iteration of the algorithm."""
         offspring = []
 
         while len(offspring) < self.population_size:
@@ -207,8 +228,7 @@ class OutputSpaceExplorationAlgorithm(AbstractGeneticAlgorithm):
 
 
 def get_index_for_solution(solution, grid_spec):
-    """Maps the objectives to the key for the grid cell
-    into which this solution falls.
+    """Maps the objectives to the key for the grid cell into which this solution falls.
 
     Parameters
     ----------
@@ -230,8 +250,7 @@ def get_index_for_solution(solution, grid_spec):
 
 
 def get_bin_index(value, minumum_value, epsilon):
-    """Maps the value for a single objective to the index
-    of the grid cell along that diemnsion
+    """Maps the value for a single objective to the index of the grid cell along that dimension.
 
     Parameters
     ----------
@@ -248,8 +267,7 @@ def get_bin_index(value, minumum_value, epsilon):
 
 
 class OutputSpaceExploration(AdaptiveTimeContinuation):
-    """Basic genetic algorithm for output space exploration using novelty
-    search.
+    """Basic genetic algorithm for output space exploration using novelty search.
 
     Parameters
     ----------
@@ -288,10 +306,11 @@ class OutputSpaceExploration(AdaptiveTimeContinuation):
         problem,
         grid_spec=None,
         population_size=100,
-        generator=RandomGenerator(),
+        generator=RandomGenerator(), # noqa: B008
         variator=None,
         **kwargs,
     ):
+        """Init."""
         super().__init__(
             OutputSpaceExplorationAlgorithm(
                 problem,
@@ -356,10 +375,11 @@ class AutoAdaptiveOutputSpaceExploration(AdaptiveTimeContinuation):
         problem,
         grid_spec=None,
         population_size=100,
-        generator=RandomGenerator(),
+        generator=RandomGenerator(), # noqa: B008
         variator=None,
         **kwargs,
     ):
+        """Init."""
         self.problem = problem
 
         # Parameterization taken from
