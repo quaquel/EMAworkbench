d7101b0d6eb9f38f47ade92755e334ae55ad98ec
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-09-23 16:21:21 +0200

    unit tests

--- test/test_em_framework/test_samplers.py
+++ test/test_em_framework/test_samplers.py
@@ -5,6 +5,7 @@
 # .. codeauthor:: jhkwakkel <j.h.kwakkel (at) tudelft (dot) nl>
 
 import numpy as np
+import pytest
 
 from ema_workbench.em_framework import Model
 from ema_workbench.em_framework.parameters import (
@@ -21,6 +22,7 @@ from ema_workbench.em_framework.samplers import (
     sample_levers,
     sample_parameters,
     sample_uncertainties,
+    DesignIterator,
 )
 
 
@@ -32,13 +34,16 @@ def test_lhs_sampler():
     ]
 
     sampler = LHSSampler()
-    samples = sampler.generate_samples(uncertainties, 100, rng=np.random.default_rng(42))
+    samples = sampler.generate_samples(
+        uncertainties, 100, rng=np.random.default_rng(42)
+    )
 
-    assert samples.shape == (100,3)
+    assert samples.shape == (100, 3)
 
     for i, u in enumerate(uncertainties):
         assert np.all(samples[:, i] >= u.lower_bound)
         assert np.all(samples[:, i] <= u.upper_bound)
+
 
 def test_mc_sampler():
     uncertainties = [
@@ -48,12 +53,15 @@ def test_mc_sampler():
     ]
 
     sampler = MonteCarloSampler()
-    samples = sampler.generate_samples(uncertainties, 100, rng=np.random.default_rng(42))
+    samples = sampler.generate_samples(
+        uncertainties, 100, rng=np.random.default_rng(42)
+    )
 
     assert samples.shape == (100, 3)
     for i, u in enumerate(uncertainties):
         assert np.all(samples[:, i] >= u.lower_bound)
         assert np.all(samples[:, i] <= u.upper_bound)
+
 
 def test_ff_sampler():
     uncertainties = [
@@ -65,10 +73,11 @@ def test_ff_sampler():
     sampler = FullFactorialSampler()
     samples = sampler.generate_samples(uncertainties, 100)
 
-    assert samples.shape == (100*11*3, 3)
+    assert samples.shape == (100 * 11 * 3, 3)
     for i, u in enumerate(uncertainties):
         assert np.all(samples[:, i] >= u.lower_bound)
         assert np.all(samples[:, i] <= u.upper_bound)
+
 
 def test_determine_parameters(mocker):
     function = mocker.Mock()
@@ -95,6 +104,7 @@ def test_determine_parameters(mocker):
     assert "b" in parameters.keys()
     assert "c" not in parameters.keys()
     assert "a" not in parameters.keys()
+
 
 def test_sample_parameters():
     parameters = [
@@ -138,6 +148,7 @@ def test_sample_uncertainties(mocker):
     assert samples.n == 100
     assert samples.kind is Scenario
 
+
 def test_sample_levers(mocker):
     function = mocker.Mock()
     model = Model("A", function)
@@ -152,3 +163,65 @@ def test_sample_levers(mocker):
 
     assert samples.n == 100
     assert samples.kind is Policy
+
+
+def test_design_iterator():
+    rng = np.random.default_rng(42)
+
+    samples = rng.uniform(size=(10, 3))
+    parameters = [RealParameter(entry, 0, 1) for entry in "abc"]
+    iterator = DesignIterator(samples, parameters, Point)
+
+    # basic intit
+    assert iterator.n == samples.shape[0]
+
+    # iteration
+    it = iter(iterator)
+    for i, entry in enumerate(it):
+        values = np.array([entry[k] for k in "abc"])
+        assert np.all(values == samples[i])
+
+    # combine
+    ## full factorial
+    samples1 = np.array([0, 1]).reshape(2, 1)
+    samples2 = np.array([0, 1]).reshape(2, 1)
+
+    it1 = DesignIterator(samples1, [RealParameter("a", 0, 1)], Point)
+    it2 = DesignIterator(samples2, [RealParameter("a", 0, 1)], Point)
+
+    it3 = it1.combine(it2, "full_factorial")
+    samples = it3.samples
+
+    assert np.all(samples == np.array([[0, 0], [0, 1], [1, 0], [1, 1]]))
+
+    ## equal length, so no need to cycle
+    samples1 = np.array([0, 1]).reshape(2, 1)
+    samples2 = np.array([0, 1]).reshape(2, 1)
+
+    it1 = DesignIterator(samples1, [RealParameter("a", 0, 1)], Point)
+    it2 = DesignIterator(samples2, [RealParameter("a", 0, 1)], Point)
+
+    it3 = it1.combine(it2, "cycle")
+    samples = it3.samples
+    assert np.all(samples == np.array([[0, 0], [1, 1]]))
+
+    ## cycle
+    samples1 = np.array([0, 1, 2]).reshape(3, 1)
+    samples2 = np.array([0, 1]).reshape(2, 1)
+
+    it1 = DesignIterator(samples1, [RealParameter("a", 0, 1)], Point)
+    it2 = DesignIterator(samples2, [RealParameter("a", 0, 1)], Point)
+
+    it3 = it1.combine(it2, "cycle")
+    samples = it3.samples
+    assert np.all(samples == np.array([[0, 0], [1, 1], [2, 0]]))
+
+    ## sample
+    it3 = it1.combine(it2, "sample", rng=42)
+    samples = it3.samples
+    assert np.all(
+        samples == np.array([[0, 0], [1, 1], [2, 1]])
+    )  # return has been hard coded for rng 42
+
+    with pytest.raises(ValueError):
+        it1.combine(it2, "wrong value")
