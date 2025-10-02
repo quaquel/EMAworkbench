974cd538f4ed27d2ea6e2ea412ca0a79685423c1
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-10-02 16:06:41 +0200

    ruff fixes connector init

--- ema_workbench/connectors/__init__.py
+++ ema_workbench/connectors/__init__.py
@@ -1,3 +1,5 @@
+"""Connectors for various modeling packages."""
+
 import warnings
 from contextlib import contextmanager
 
@@ -8,36 +10,37 @@ warnings.simplefilter("once", ImportWarning)
 
 @contextmanager
 def catch_and_ignore_import_warning():
+    """Helper context manager to catch and ignore import warnings."""
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=ImportWarning)
         yield
 
 
 try:
-    from . import vensim
+    from . import vensim  # noqa F401
 except ImportError:
-    warnings.warn("vensim connector not available", ImportWarning)
+    warnings.warn("vensim connector not available", ImportWarning, stacklevel=2)
 
 try:
-    from . import excel
+    from . import excel  # noqa F401
 except ImportError:
-    warnings.warn("excel connector not available", ImportWarning)
+    warnings.warn("excel connector not available", ImportWarning, stacklevel=2)
 
 try:
-    from . import netlogo
+    from . import netlogo  # noqa F401
 except ImportError:
-    warnings.warn("netlogo connector not available", ImportWarning)
+    warnings.warn("netlogo connector not available", ImportWarning, stacklevel=2)
 
 try:
-    from . import simio_connector
+    from . import simio_connector  # noqa F401
 except ImportError:
-    warnings.warn("simio connector not available", ImportWarning)
+    warnings.warn("simio connector not available", ImportWarning, stacklevel=2)
 except EMAError:
-    warnings.warn("simio not found, connector not available", ImportWarning)
+    warnings.warn("simio not found, connector not available", ImportWarning, stacklevel=2)
 
 
 with catch_and_ignore_import_warning():
     try:
-        from . import pysd_connector
+        from . import pysd_connector  # noqa F401
     except ImportError:
-        warnings.warn("pysd connector not available", ImportWarning)
+        warnings.warn("pysd connector not available", ImportWarning, stacklevel=2)
